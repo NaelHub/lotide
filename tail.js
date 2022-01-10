efc0c06b70b8f7fa6a38ed122082ba9b84ceb6a0
NaelHub <nael.hub@gmail.com>
2022-01-10 23:42:48 +0000

    Updates

--- tail.js
+++ tail.js
@@ -1,11 +1,15 @@
-const assertEqual = require(`./assertEqual`) 
-const tail = function(array)
-{
+const assertEqual = require(`./assertEqual`)
+const tail = function (array) {
     array2 = array.slice[1]
-    return(array2)
+    return (array2)
 }
 // TEST CODE
 //assertEqual("Lighthouse Labs", "Bootcamp");
 //assertEqual(1, 1); 
 // Test Case: Check the original array 
 module.exports = tail;
+
+/*
+[1, 2, 3] === [1, 2, 3] // => false
+[1, 2, 3] == [1, 2, 3] // => false
+*/
