d12572315c9e8ccc2b1d4c6cc72547aaa5da7223
NaelHub <nael.hub@gmail.com>
2022-01-10 19:13:29 +0000

    testing

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,12 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected){
-      console.log(`These two are the same: ${actual} vs ${expected}`)
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`These two are the same: ${actual} vs ${expected}`)
   } else {
-      console.log(`These two are not the same: ${actual} vs ${expected}`);
+    console.log(`These two are not the same: ${actual} vs ${expected}`);
   }
 
 };
 
 module.exports = assertEqual;
+
+//testing testing
